e92bd10d11dc24ba9565394ecef0816c9175dc33
Navneet Vishwakarma <rajvl132011@gmail.com>
2025-01-24 00:11:05 +0530

    It is also working

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -194,9 +194,6 @@ const Home = () => {
             </div>
             <div className='flex flex-col justify-end h-screen absolute top-0 w-full'>
                 <div className='h-[30%] p-6 bg-white relative'>
-                    <h5 ref={panelCloseRef} onClick={() => setPanelOpen(false)} className='absolute opacity-0 right-6 top-6 text-2xl'>
-                        <i className="ri-arrow-down-wide-line"></i>
-                    </h5>
                     <h4 className='text-2xl font-semibold'>Find a trip</h4>
                     <form className='relative py-3' onSubmit={submitHandler}>
                         <div className="line absolute h-16 w-1 top-[50%] -translate-y-1/2 left-5 bg-gray-700 rounded-full"></div>
@@ -244,9 +241,13 @@ const Home = () => {
                             placeholder='Enter your destination'
                         />
                     </form>
-                    <button onClick={findTrip} className='bg-black text-white px-4 py-2 rounded-lg mt-3 w-full'>
+                    <button onClick={findTrip} className='bg-black text-white px-4 py-2 rounded-lg mt-3 w-full '>
                         Find Trip
                     </button>
+                    <button onClick={()=>{setPanelOpen(false)}} className='bg-red-600 text-white px-4 py-2 rounded-lg mt-3 w-full'>
+                        Close
+                    </button>
+
                 </div>
                 <div ref={panelRef} className='bg-white h-0'>
                     <LocationSearchPanel
