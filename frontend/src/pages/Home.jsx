e8778ff5102dafaf8100c19cca748f369463eade
Navneet Vishwakarma <rajvl132011@gmail.com>
2025-01-26 22:37:04 +0530

    waitingfor driver

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -342,7 +342,7 @@ const Home = () => {
                     setVehicleFound={setVehicleFound}
                 />
             </div>
-            <div ref={waitingForDriverRef} className='fixed w-full z-10 bottom-0 bg-white px-3 py-6 pt-12'>
+            <div ref={waitingForDriverRef} className='fixed w-full z-10 bottom-0 translate-y-full bg-white px-3 py-6 pt-12'>
                 <WaitingForDriver
                     ride={ride}
                     setVehicleFound={setVehicleFound}
