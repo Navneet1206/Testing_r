358e7f27228a3fb9fb04108e3605b9e7351c2659
Navneet Vishwakarma <rajvl132011@gmail.com>
2025-02-06 19:06:29 +0530

    vercel

--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -1,7 +1,7 @@
 const userModel = require("../models/user.model");
 const userService = require("../services/user.service");
 const { validationResult } = require("express-validator");
-const blackListTokenModle = require("../models/blackListToken.model");
+const blackListTokenModel = require("../models/blackListToken.model");
 const { generateOTP } = require("../utils/otp.utils");
 const { sendEmailOTP, sendSMSOTP } = require("../services/communication.service");
 
